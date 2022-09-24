195e57062e85dd0d24d5fb6f5d4ad581c521275a
shobhit02 <shobhit02@gmail.com>
2022-09-24 16:56:06 +0530

    React training
    Training Day 4

--- my-app/App.js
+++ my-app/App.js
@@ -1,7 +1,7 @@
 //import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Demo from './Demo';
-import DemoState from './DemoState';
+import DemoState from './demoState';
 
 
 export default function App() {
