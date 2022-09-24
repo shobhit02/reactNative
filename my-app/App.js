5d8b0bc53c2f907473be765b5590cc81b11c5199
shobhit02 <shobhit02@gmail.com>
2022-09-24 16:32:10 +0530

    Day 4
    Demo

--- my-app/App.js
+++ my-app/App.js
@@ -1,11 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
+//import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Demo from './Demo';
+
 
 export default function App() {
+ 
+      // Arry of data here 
+
+         let companies=['Birlasoft','Accenture','Barclays','wipro']
+      
+  
+        // define one more sytle in this component 
+
+        let mystyle={
+            
+                     fontSize: 50,
+                     color:'#FF0000'
+        } 
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center"}}>
+      <Text>
+        <p style={mystyle}>Hello I am Shobhit Jain! easy go 🎉</p>
+        <p>{2+2}</p>
+      </Text>
+      <Demo name="Shobhit"></Demo>
+
+      <Text>
+          <ul>
+              {companies.map(function(company,index){
+
+                return <li key={index}>{company}</li>
+
+              })}
+
+          </ul>
+      </Text>
+
     </View>
   );
 }
